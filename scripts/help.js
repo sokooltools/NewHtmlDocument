e7829b558bb88cd18b66ae71d35f752de046bc8b
SokoolTools <57449110+sokooltools@users.noreply.github.com>
2019-12-06 21:37:00 -0500

    Modified   scripts/help.js Modified   themes/help.css

--- scripts/help.js
+++ scripts/help.js
@@ -57,10 +57,10 @@ $(document).ready(function() {
 	});
 
 	// Add a tooltip to the 'Go to Top' button.
-	$("#help_btnScrollToTop").attr('title', "Click to go to the top of this page...");
+	$("#help_btnScrollToTop").attr('title', "Click to go to the 'Table of Contents'.");
 
 	// Add a tooltip to the 'Close' button.
-	$("#help_btnClose").attr('title', "Click to close this online help...");
+	$("#help_btnClose").attr('title', "Click to close this online help.");
 
 	// Add event to close the window when the 'Close' button is clicked.
 	$("#help_btnClose").click(function() {
@@ -78,11 +78,11 @@ $(document).ready(function() {
 		if  ($(window).height() + $(window).scrollTop() !== $(document).height())
 		{
 			$('html, body').animate({
-				scrollTop: "-=40"
+				scrollTop: "-=56"
 			}, "fast");
 		}
 	};
-
+	
 	// -------------------------------------------------------------------------------------------
 	// Shows the name of each image on the page. (This is for debug and thus executed manually).
 	// -------------------------------------------------------------------------------------------
@@ -90,11 +90,42 @@ $(document).ready(function() {
 		if ($("div").hasClass("centered"))
 			return;
 		const sel1 = $("img");
-		const sel2 = $("[src$='.jpg']");
+		const sel2 = $("[src$='.png']");
 		for (let i = 0; i < sel1.length; i++) {
 			sel1[i].outerHTML += `<div class='centered'> [${sel2[i].src.substring(sel2[i].src.lastIndexOf("/") + 1)}]</div>`;
 		}
 		window.console.log(`Now showing ${sel2.length} image names...`);
 	};
+
+	$("body").append("<a href='#' title='Click to go to the top of this page...' id='hb-gotop' style='display:none;'>Scroll to Top</a>");
+
+	$.fn.scrollToTop = function()
+	{
+		$(this).hide().removeAttr("href");
+		if ($(window).scrollTop() !== "0") {
+			$(this).fadeIn("slow");
+		}
+		var scrollDiv = $(this);
+		$(window).scroll(
+			function()
+			{
+				if ($(window).scrollTop() === "0") {
+					$(scrollDiv).fadeOut("slow");
+				}
+				else {
+					$(scrollDiv).fadeIn("slow");
+				}
+			}
+		);
+		$(this).on("click", 
+			function() {
+				$("html, body").animate(
+				{
+					scrollTop: 0
+				}, "slow");
+			}
+		);
+	}
+	$("#hb-gotop").scrollToTop();
 
 });
