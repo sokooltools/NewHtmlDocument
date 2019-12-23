96be785f2c567c6bf281d03429a6c138a0b7efca
sokooltools <ms@sokool.net>
2019-12-23 11:49:48 -0500

    Enhanced the html, javascript, and css;

--- scripts/help.js
+++ scripts/help.js
@@ -3,7 +3,43 @@
 // =======================================================================================================
 $(document).ready(function() {
 
-	// Add 'Table of Contents' to the page.
+	// Add tooltips.
+	$("#help_btnScrollToTop").attr('title', "Click to go to the 'Table of Contents' at the top of this page.");
+	$("#help_btnShowCommon").prop("title", 'Click to show a help page common to all help pages.');
+	$("#help_btnClose").prop("title", "Click to close this help page.");
+		
+	$("#help_btnShowCommon").text("Go to 'Common' Help");
+
+	$("#help_btnShowCommon").on("click", function() {
+		var pA = window.location.pathname.split("/");
+		location.href = "common.htm?tok=" + document.title + "&ret=" + pA[pA.length - 1];
+		return false;
+	});
+	
+	var pg = getQueryStringByName("tok");
+	$("#help_btnGoBack").text("Back to " + pg + "" ).prop("title", "Click to go back to the " + pg + " page.");
+
+	$("#help_btnGoBack").on("click", function() {
+		location.href = getQueryStringByName("ret");
+	});
+
+	$("#help_Toolbar").on("click", function(e) {
+		if (e.ctrlKey)
+			showImageNames();
+		return false;
+	});
+
+	$("#help_btnClose").on("click", function() {
+		window.close();
+	});
+
+	$("#help_btnScrollToTop, .li2 > a").on("click", function() {
+		window.scrollTo(0, 0);
+	});
+
+	// -------------------------------------------------------------------------------------------
+	// Creates and automatically adds a 'Table of Contents' to the page.
+	// -------------------------------------------------------------------------------------------
 	(function createTableOfContents() {
 		var sel = $("LI.li2 > a");
 		sel.attr('title', "Click to go to the top of this page...");
@@ -49,28 +85,6 @@ $(document).ready(function() {
 			scrollToElement(this.hash.replace('#',''));
 		});
 	})();
-	
-	$("#help_Toolbar").on("click", function(e) {
-		if (e.ctrlKey)
-			showImageNames();
-		return false;
-	});
-
-	// Add a tooltip to the 'Go to Top' button.
-	$("#help_btnScrollToTop").attr('title', "Click to go to the top of this page...");
-
-	// Add a tooltip to the 'Close' button.
-	$("#help_btnClose").attr('title', "Click to close this online help...");
-
-	// Add event to close the window when the 'Close' button is clicked.
-	$("#help_btnClose").click(function() {
-		window.close();
-	});
-
-	// Add event to scroll to top of page when any caption is clicked.
-	$("#help_btnScrollToTop, .li2 > a").click(function() {
-		window.scrollTo(0, 0);
-	});
 
 	// Scroll the selected topic into view adjusting for the header bar.
 	function scrollToElement(elId) {
@@ -78,11 +92,11 @@ $(document).ready(function() {
 		if  ($(window).height() + $(window).scrollTop() !== $(document).height())
 		{
 			$('html, body').animate({
-				scrollTop: "-=40"
+				scrollTop: "-=56"
 			}, "fast");
 		}
 	};
-
+	
 	// -------------------------------------------------------------------------------------------
 	// Shows the name of each image on the page. (This is for debug and thus executed manually).
 	// -------------------------------------------------------------------------------------------
@@ -90,11 +104,52 @@ $(document).ready(function() {
 		if ($("div").hasClass("centered"))
 			return;
 		const sel1 = $("img");
-		const sel2 = $("[src$='.jpg']");
+		const sel2 = $("[src$='.png'],[src$='.jpg']");
 		for (let i = 0; i < sel1.length; i++) {
 			sel1[i].outerHTML += `<div class='centered'> [${sel2[i].src.substring(sel2[i].src.lastIndexOf("/") + 1)}]</div>`;
 		}
 		window.console.log(`Now showing ${sel2.length} image names...`);
 	};
 
+	$("body").append("<a href='#' title='Click to scroll to the top of this page...' id='hb-gotop' style='display:none;'>Scroll to Top</a>");
+
+	$.fn.scrollToTop = function()
+	{
+		$(this).hide().removeAttr("href");
+		if ($(window).scrollTop() !== "0") {
+			$(this).fadeIn("slow");
+		}
+		var scrollDiv = $(this);
+		$(window).scroll(
+			function()
+			{
+				if ($(window).scrollTop() === "0") {
+					$(scrollDiv).fadeOut("slow");
+				}
+				else {
+					$(scrollDiv).fadeIn("slow");
+				}
+			}
+		);
+		$(this).on("click", 
+			function() {
+				$("html, body").animate(
+				{
+					scrollTop: 0
+				}, "slow");
+			}
+		);
+	}
+	$("#hb-gotop").scrollToTop();
+
 });
+
+// -------------------------------------------------------------------------------------------
+// Returns the query string content corresponding to the specified name.
+// -------------------------------------------------------------------------------------------
+function getQueryStringByName(name) {
+	name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+	var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+		results = regex.exec(location.search);
+	return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+};
